e11440f0cf8e3881c627c0c535fc0bcb052298ac
WikiDreams <danielsantosvieiramail@gmail.com>
2016-05-01 22:22:00 +0100

    update

---
+++ JavaScript/getAllFiles.js
@@ -0,0 +1,14 @@
+'use strict';
+function getFiles(dir){
+    fileList = [];
+
+    var files = fs.readdirSync(dir);
+    for(var i in files){
+        if (!files.hasOwnProperty(i)) continue;
+        var name = dir+'/'+files[i];
+        if (!fs.statSync(name).isDirectory()){
+            fileList.push(name);
+        }
+    }
+    return fileList;
+}
